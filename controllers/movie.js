236e671f72790453a11c35731c5cc8e35230bfb0
Efim Shchegolev <efimblashck@yandex.ru>
2023-03-17 11:48:01 +0300

    feat: проверка исправления удаления 2

--- controllers/movie.js
+++ controllers/movie.js
@@ -56,10 +56,10 @@ module.exports.createMovie = (req, res, next) => {
 };
 
 module.exports.deleteMovie = (req, res, next) => {
-  Movie.findById(req.params.movieId)
+  Movie.findOne({ movieId: req.params.movieId })
     .orFail(new NotFoundError(FILM_NOT_FOUND_ERROR_TEXT))
     .then((movie) => {
-      // // res.send({ owner: movie.owner.toString(), movie: req.user._id });
+      // res.send({ owner: movie.owner.toString(), movie: req.user._id });
       // if (movie.owner.toString() === req.user._id) {
       //   return movie.remove()
       //     .then(() => res.send({ message: FILM_DELETE_TEXT }))
